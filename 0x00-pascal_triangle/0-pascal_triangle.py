06247b8dad75dc95a1c1e88179ff4fb0e373c53a
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-07-02 14:06:34 +0100

    committing 0-pascal_triangle.py task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/user/bin/python3
+"""A script that prints the Pascal's triangle for any number"""
+
+
+def pascal_triangle(n):
+    """A function that returns a list of lists of integers
+    representing the Pascal’s triangle of n"""
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
