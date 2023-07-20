015be5183ad6fa3809fcba24aaba8bbe2e229c9a
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-07-20 23:12:42 +0100

    fix task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,63 @@
 #!/usr/bin/python3
-'''Log parsing'''
-import sys
-import signal
+'''Write a script that reads stdin line by line and computes metrics:
 
-status_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
-file_size_total = 0
-status_code_counts = {code: 0 for code in status_codes}
-line_count = 0
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size> (if the format is not this one, the line must
+be skipped)
+After every 10 lines and/or a keyboard interruption (CTRL + C), print these
+statistics from the beginning:
+Total file size: File size: <total size>
+where <total size> is the sum of all previous <file size>
+(see input format above)
+Number of lines by status code:
+possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+if a status code doesn’t appear or is not an integer, don’t print anything
+for this status code
+format: <status code>: <number>
+status codes should be printed in ascending order
+Warning: In this sample, you will have random value - it’s normal to not
+have the same output as this one.
+'''
+# store the count of all status codes in a dictionary
+status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                     '404': 0, '405': 0, '500': 0}
 
+total_size = 0
+count = 0  # keep count of the number lines counted
 
-def print_statistics():
-    '''Prints the statistics'''
-    print("Total file size:", file_size_total)
-    for code in sorted(status_codes, key=int):
-        count = status_code_counts[code]
-        if count > 0:
-            print(f"{code}: {count}")
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
 
+        if len(line_list) > 4:
+            status_code = line_list[-2]
+            file_size = int(line_list[-1])
 
-def signal_handler(sig, frame):
-    '''Handles the SIGINT signal'''
-    print_statistics()
-    sys.exit(0)
+            # check if the status code receive exists in the dictionary and
+            # increment its count
+            if status_code in status_codes_dict.keys():
+                status_codes_dict[status_code] += 1
 
+            # update total size
+            total_size += file_size
 
-signal.signal(signal.SIGINT, signal_handler)
+            # update count of lines
+            count += 1
 
-if __name__ == '__main__':
-    try:
-        for line in sys.stdin:
-            line_count += 1
-            if line_count % 10 == 0:
-                print_statistics()
+        if count == 10:
+            count = 0  # reset count
+            print('File size: {}'.format(total_size))
 
-            parts = line.strip().split()
-            if len(parts) != 10:
-                continue
+            # print out status code counts
+            for key, value in sorted(status_codes_dict.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-            ip, date, method, path, protocol, status_code, file_size = parts
-            if not status_code.isdigit() or status_code not in status_codes:
-                continue
+except Exception as err:
+    pass
 
-            file_size_total += int(file_size)
-            status_code_counts[status_code] += 1
-
-    except KeyboardInterrupt:
-        pass
-
-    print_statistics()
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(status_codes_dict.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
