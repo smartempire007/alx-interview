90b083868752946a782d54965addc210ba814d09
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-07-26 22:33:36 +0100

    task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,33 +3,24 @@
 
 
 def validUTF8(data):
-    """Determines if a given data set represents a
-    valid UTF-8 encoding"""
+    """Determines if a given data set represents a valid UTF-8 encoding"""
+    if not data:
+        return False
 
-    def check_next_bytes(num_bytes):
-        nonlocal i
-        for _ in range(num_bytes):
-            if i >= len(data) or (data[i] >> 6) != 0b10:
-                return False
-            i += 1
-        return True
-
-    i = 0
-    while i < len(data):
-        byte = data[i]
-        if (byte >> 7) == 0:  # 1-byte character (0xxxxxxx)
-            i += 1
-        elif (byte >> 5) == 0b110:  # 2-byte character (110xxxxx)
-            if not check_next_bytes(1):
-                return False
-        elif (byte >> 4) == 0b1110:  # 3-byte character (1110xxxx)
-            if not check_next_bytes(2):
-                return False
-        elif (byte >> 3) == 0b11110:  # 4-byte character (11110xxx)
-            if not check_next_bytes(3):
+    n_bytes = 0
+    for num in data:
+        bin_rep = format(num, '#010b')[-8:]
+        if n_bytes == 0:
+            for bit in bin_rep:
+                if bit == '0':
+                    break
+                n_bytes += 1
+            if n_bytes == 0:
+                continue
+            if n_bytes == 1 or n_bytes > 4:
                 return False
         else:
-            return False
-        i += 1
-
-    return True
+            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
+                return False
+        n_bytes -= 1
+    return n_bytes == 0
