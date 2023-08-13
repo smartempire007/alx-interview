2404dceaf47e8f252a37c701a1968b8e96fd9061
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-08-13 23:44:30 +0100

    fix on star war task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 /**
  * Prints all characters of a Star Wars movie.
- * The first positional argument passed is the Movie ID, which corresponds to a specific Star Wars movie.
- * Displays one character name per line in the same order as the "characters" list in the /films/ endpoint.
- * Uses the Star Wars API (https://swapi-api.hbtn.io) to fetch movie and character data.
+ * The first positional argument passed is the Movie ID,
+ * which corresponds to a specific Star Wars movie.
+ * Displays one character name per line in the same order
+ * as the "characters" list in the /films/ endpoint.
+ * Uses the Star Wars API (https://swapi-api.hbtn.io)
+ * to fetch movie and character data.
  * Utilizes the request-promise module for making HTTP requests.
  */
 
@@ -24,9 +27,12 @@ const fetchData = async () => {
     const characters = filmJson.characters;
 
     // Fetch character data using promises
-    const characterPromises = characters.map(character => request(character));
-    const characterResponses = await Promise.all(characterPromises);
-    const characterData = characterResponses.map(response => JSON.parse(response));
+    const characterPromises = characters.map(
+        character => request(character));
+    const characterResponses = await Promise.all(
+        characterPromises);
+    const characterData = characterResponses.map(
+        response => JSON.parse(response));
 
     // Print character names
     for (const character of characterData) {
