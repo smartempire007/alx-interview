d7e113fd4c0b882765e9e23c78ac70a56eb0ccf8
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-08-17 13:07:41 +0100

    fix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,23 +1,28 @@
 def rotate_2d_matrix(matrix):
-    """
-    Rotates an n x n 2D matrix 90 degrees clockwise.
+    """Rotates a 2D matrix 90 degrees clockwise.
 
     Args:
-        matrix (list[list]): The input matrix.
+        matrix (list[list]): The input matrix to be rotated.
 
     Returns:
         None: The matrix is modified in-place.
     """
-    n = len(matrix)
-    for i in range(n // 2):
-        for j in range(i, n - i - 1):
-            # Store top-left element
-            temp = matrix[i][j]
-            # Move bottom-left element to top-left
-            matrix[i][j] = matrix[n - 1 - j][i]
-            # Move bottom-right element to bottom-left
-            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
-            # Move top-right element to bottom-right
-            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
-            # Move top-left (stored) element to top-right
-            matrix[j][n - 1 - i] = temp
+
+    n = len(matrix)  # Get the size of the matrix
+
+    copy = matrix[:]  # Create a copy of the original matrix
+
+    # Iterate through each element in the matrix
+    for y in range(n):
+        dx = []
+
+        # Iterate through each column in reverse order
+        for x in range(n-1, -1, -1):
+            # Append elements from each column to a new row list (dx)
+            dx.append(matrix[x][y])
+        # Add the new row list (dx) to the original matrix
+        matrix.append(dx)
+
+    # Remove the original rows from the matrix
+    for dx in copy:
+        matrix.pop(matrix.index(dx))
